c555b15e33de7fb9ce241eff28d38ab719a19f87
Marie Sonne <71631052+mariesonne@users.noreply.github.com>
2020-11-30 10:25:07 +0100

    Navbar i PHP-fil
    Jeg har flyttet navbaren i PHP for sig selv og jeg har ikke linket til den i min index.html endnu.

---
+++ includes/header.php
@@ -0,0 +1,82 @@
+    <div class="d">
+        <nav class="navbar navbar-expand-lg navbar-light bg-custom">
+          
+            <a class="navbar-brand" href="#">Marie Sonne <span>l æ r i n g s p o r t f o l i o</span></a>
+              
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            
+              <ul class="navbar-nav ml-auto">
+                  
+                   <li class="nav-item dropdown">
+                      
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Webudvikling
+                        </a>
+                      
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a class="dropdown-item" href="#">Internettets historie</a>
+                          <a class="dropdown-item" href="#">Serverside</a>
+                          <a class="dropdown-item" href="#">HTML</a>
+                            <a class="dropdown-item" href="#">CSS</a>
+                            <a class="dropdown-item" href="#">Versionsstyring</a>
+                            <a class="dropdown-item" href="#">Frameworks</a>
+                        </div>
+                       
+                    </li>
+                  
+                    <li class="nav-item dropdown">
+                      
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Design
+                        </a>
+                      
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                        
+                    </li>
+                  
+                    <li class="nav-item dropdown">
+                      
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Kommunikation
+                        </a>
+                      
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+      
+                    </li>
+                  
+                     <li class="nav-item dropdown">
+                      
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Projekter
+                        </a>
+                      
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+      
+                    </li>
+                  
+                    <li class="nav-item">
+                    
+                        <a class="nav-link" id="link" href="#">Link</a>
+                    </li>
+                  
+                </ul>
+            </div>
+
+        </nav>
+        </div>
